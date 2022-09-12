7b99785758652afe67be66aa6c1ad5525a8a301f
Admin <jonny.kea@mail.ru>
2022-09-12 13:09:18 +1000

    Lesson03 equals&hashCode

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -4,6 +4,21 @@ package com.urise.webapp.model;
  * Initial resume class
  */
 public class Resume {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resume resume = (Resume) o;
+
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
     // Unique identifier
     private String uuid;
 
